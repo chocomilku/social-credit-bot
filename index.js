28e8a23a60a6f1c09765fee666ec528926ff9b2b
chocomilku <andrei.berja078@gmail.com>
2021-10-31 15:32:28 +0800

    added individual command and error handling

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const { Client, Intents } = require('discord.js')
+const { Client, Intents, Collection } = require('discord.js')
 const { env } = require('./globals')
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], presence: {status: 'online', activities: [{name: 'Social Credit Test', type: 'PLAYING'}]} })
 
 client.once('ready', () => { console.log(`Ready!\n${client.user.tag} is Online!\nCTRL+C to Stop this bot`) })
+
+client.commands = new Collection()
+const commandFiles = fs.readdirSync('./commands')
+  .filter(file => 
+    file.endsWith('.js')
+    )
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.data.name, command)
+}
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+  
+    const command = client.commands.get(interaction.commandName)
+  
+    if (!command) return
+  
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      const d = new Date();
+      console.error(`${d.toLocaleString()}: ${error} [${interaction.options.getString('search')}]`)
+      const embedError = new MessageEmbed()
+        .setColor('RED')
+        .setTitle(`${error}`)
+        .setTimestamp(d)
+      await interaction.Reply({ content: `An Error has Occured. Please try again`, embeds: [embedError]})
+      console.error(error)
+    }
+  })
 
 client.login(env.token)
